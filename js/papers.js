b6c9e083ef98d694d6827c8c6d3026ccbce1feb9
Richard Mortier <mort@cantab.net>
2011-07-17 00:09:27 +0100

    [papers] new world- locally served json

--- js/papers.js
+++ js/papers.js
@@ -1,58 +1,4 @@
 var papers = (function () {
-
-    var bibfiles = [ 
-//        "https://github.com/mor1/rmm-bibs/blob/master/strings.bib",
-            "https://github.com/mor1/rmm-bibs/blob/master/rmm-conference.bib",
-            "https://github.com/mor1/rmm-bibs/blob/master/rmm-workshop.bib",
-            "https://github.com/mor1/rmm-bibs/blob/master/rmm-journal.bib",
-            "https://github.com/mor1/rmm-bibs/blob/master/rmm-patents.bib",
-            "https://github.com/mor1/rmm-bibs/blob/master/rmm-techreport.bib",
-            "https://github.com/mor1/rmm-bibs/blob/master/rmm-unpublished.bib"
-    ];
-    var promises = [];
-    var entries  = [];
-
-    parse = function (input) {
-        var raw = input
-            .replace(/\<div\>|\<\/div\>/gm,"")
-            .replace(/  |\n/gm," ");
-        var bt = new BibTex();
-        bt.content = raw;
-        bt.parse();
-        $(bt.data).each(
-            function(i,v) {
-                $(v.author).each(
-                    function (ii,vv) {
-                        if (/ |~/.test(vv.last)) {
-                            var ns = vv.last.split(/ |~/);
-                            vv.first = ns[0];
-                            vv.last = ns.slice(1,ns.length).join(" ");
-                        }
-                    });
-                entries.push(v);
-            });
-    };
-
-    fetch = function (_, url) {
-        promises.push(
-            $.Deferred(
-                function (promise) {
-/*
-                    $.yql('select * from html where url=@url and xpath=@xpath',
-                          {
-                              'url': url,
-                              'xpath': "//div[@class=\'highlight']/pre"
-                          },
-                          function (data) {
-                              parse(data.query.results.pre.content);
-                              promise.resolve();
-                          });
- */
-                }
-            ).promise()
-        );
-    };
-
     render_entry = function (_, entry) {
         var format_author = function (a) {
             return a.first[0]+". "+a.last;
@@ -71,18 +17,21 @@ var papers = (function () {
 
     return {
         render : function () {
-            $.each(bibfiles, fetch);
-            $.when.apply(null, promises).then(
-                function () {
-                    $.each(entries, render_entry);
-                });
-        }
-    };
-})();
-
-$.getJSON(
-    "https://api.github.com/repos/swanson/swanson.github.com/git/trees/master",
+            $.getJSON(
+    "/research/papers/authors.json",
 //    "https://raw.github.com/mor1/rmm-bibs/master/rmm-conference.bib?callback=?",
     function (data) {
         console.log(data);
     });
+$.getJSON(
+    "/research/papers/papers.json",
+//    "https://raw.github.com/mor1/rmm-bibs/master/rmm-conference.bib?callback=?",
+    function (data) {
+        console.log(data);
+    });
+
+        }
+    };
+})();
+
+papers.render();
--- research/papers/index.md
+++ research/papers/index.md
@@ -5,10 +5,8 @@ title: papers
 parent: mort
 ---
 
-Note: this list is somewhat out-of-date and is simply a placeholder
-for now.  [Visit my github BibTeX repo][r] for up-to-date citation
-details.  [Contact me][e] if you have any problems obtaining any of
-these.
+[Visit my github BibTeX repo][r] for up-to-date citation details.
+[Contact me][e] if you have any problems obtaining any of these. 
 
 [r]: http://github.com/mor1/rmm-bibs
 [e]: mailto:richard.mortier@nottingham.ac.uk
