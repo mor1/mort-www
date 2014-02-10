2c162878b976e61eb3d2db450c3f9fde429ecd0a
Richard Mortier <mort@cantab.net>
2014-02-10 22:12:22 +0000

    [travis] update opam remote

--- .travis-build.sh
+++ .travis-build.sh
@@ -17,8 +17,8 @@ export OPAMYES=1
 opam init
 eval `opam config env`
 
-opam remote add -k git mirage-split \
-    https://github.com/mirage/opam-repository#mirage-1.1.0
+opam remote add -k git mirage-1.1.0 \
+    https://github.com/mirage/opam-repository#mirage-1.1-release
 
 opam update -u
 
