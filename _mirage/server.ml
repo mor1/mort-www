5a3b40a2b43d6f9ecc0cf9f45c0a874a0bc8476d
Richard Mortier <mort@cantab.net>
2015-02-16 16:10:20 +0000

    mirage: just serve the static jekyll site

--- _mirage/server.ml
+++ _mirage/server.ml
@@ -16,88 +16,46 @@
  *)
 
 open Lwt
+open V1_LWT
 
-(* XXX needs to plumb the failing name through somehow *)
-let (>>>) x f =
-  x >>= function
-  | `Error _ -> fail (Failure "name")
-  | `Ok x    -> f x
+module Main (C: CONSOLE) (SITE: KV_RO) (S: Cohttp_lwt.Server) = struct
 
-module Main
-         (C: V1_LWT.CONSOLE) (S: Cohttp_lwt.Server) (ASSETS: V1_LWT.KV_RO)
-         (PAGES: V1_LWT.KV_RO) (POSTS: V1_LWT.KV_RO) (COURSES: V1_LWT.KV_RO)
-         (PAPERS: V1_LWT.KV_RO) (BIGPAPERS: V1_LWT.KV_RO)
+  let start c site http =
+    let read_site name =
+      SITE.size site name >>= function
+      | `Error (SITE.Unknown_key _) ->
+        fail (Failure ("read_site_size " ^ name))
+      | `Ok size ->
+        SITE.read site name 0 (Int64.to_int size) >>= function
+        | `Error (SITE.Unknown_key _) ->
+          fail (Failure ("read_site " ^ name))
+        | `Ok bufs -> return (Cstruct.copyv bufs)
+    in
 
-  = struct
+    let callback conn_id req body =
+      let path = req |> S.Request.uri |> Uri.path in
+      C.log c (Printf.sprintf "URL: '%s'" path);
 
-    (** Functor that produces a structure representing a unikernel given the
-        driver structures specified in [config.ml]. Instantiated via e.g.,
-        {! Lwt_unix.run} or as a Xen VM. *)
+      try_lwt
+        read_site path >>= fun body ->
+        S.respond_string ~status:`OK ~body ()
+      with
+      | Failure m ->
+        Printf.printf "CATCH: '%s'\n%!" m;
+        let cpts = path
+                   |> Re_str.(split_delim (regexp_string "/"))
+                   |> List.filter (fun e -> e <> "")
+        in
+        match cpts with
+        | [] | [""] -> S.respond_string ~status:`OK ~body:Content.body ()
+        | x -> S.respond_not_found ~uri:(S.Request.uri req) ()
+    in
 
-    let start c http assets pages posts courses papers bigpapers =
-      (** Unikernel entry point. *)
+    let conn_closed (_, conn_id) =
+      let cid = Cohttp.Connection.to_string conn_id in
+      C.log c (Printf.sprintf "conn %s closed" cid)
+    in
 
-      (** First, project all the required methods we'll need from the Mirage
-          Driver modules. *)
+    http (S.make ~callback ~conn_closed ())
 
-      let http_respond_ok ?(headers=[]) body =
-        body >>= fun body ->
-        let status = `OK in
-        let headers = Cohttp.Header.of_list headers in
-        S.respond_string ~headers ~status ~body ()
-      in
-      let http_respond_redirect ~uri = S.respond_redirect ~uri () in
-      let http_respond_notfound ~uri = S.respond_not_found ~uri () in
-
-      let get_asset ~name =
-        ASSETS.size assets name                       >>> fun size ->
-        ASSETS.read assets name 0 (Int64.to_int size) >>> fun bufs ->
-        return (Cstruct.copyv bufs)
-      in
-
-      let get_page ~name =
-        PAGES.size pages name                       >>> fun size ->
-        PAGES.read pages name 0 (Int64.to_int size) >>> fun bufs ->
-        return (Cow.Markdown.of_string (Cstruct.copyv bufs))
-      in
-
-      let get_post ~name =
-        POSTS.size posts name                       >>> fun size ->
-        POSTS.read posts name 0 (Int64.to_int size) >>> fun bufs ->
-        return (Cow.Markdown.of_string (Cstruct.copyv bufs))
-      in
-
-      let get_courses ~name =
-        COURSES.size courses name                       >>> fun size ->
-        COURSES.read courses name 0 (Int64.to_int size) >>> fun bufs ->
-        return (Cstruct.copyv bufs)
-      in
-
-      let get_papers ~name =
-        PAPERS.size papers name                       >>> fun size ->
-        PAPERS.read papers name 0 (Int64.to_int size) >>> fun bufs ->
-        return (Cstruct.copyv bufs)
-      in
-
-      let get_bigpapers ~name =
-        BIGPAPERS.size bigpapers name                       >>> fun size ->
-        BIGPAPERS.read bigpapers name 0 (Int64.to_int size) >>> fun bufs ->
-        return (Cstruct.copyv bufs)
-      in
-
-      let callback conn_id ?body request =
-        let uri = S.Request.uri request in
-        let unik = {
-          Unikernel.log = (fun ~msg -> C.log c msg);
-          get_asset; get_page; get_post; get_courses; get_papers; get_bigpapers;
-          http_respond_ok; http_respond_redirect; http_respond_notfound;
-        } in
-        Site.dispatch unik uri
-      in
-      let conn_closed conn_id () =
-        let cid = Cohttp.Connection.to_string conn_id in
-        C.log c (Printf.sprintf "conn %s closed" cid)
-      in
-      http { S.callback = callback; conn_closed }
-
-  end
+end
