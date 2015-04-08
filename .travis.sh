fece8cd01921744102be2d06430764b565a9b5da
Richard Mortier <mort@cantab.net>
2015-04-08 10:48:45 +0100

    travis: ruby version dep issues; try to use `rvm`

--- .travis.sh
+++ .travis.sh
@@ -21,9 +21,11 @@ set -ex
 
 ## install jekyll and build site
 
-sudo apt-get remove ruby1.8
-sudo apt-get autoremove
-sudo apt-get install ruby1.9.1 ruby1.9.1-dev rubygems1.9.1
+rvm use 2.1 --install --binary --fuzzy
+
+# sudo apt-get remove ruby1.8
+# sudo apt-get autoremove
+# sudo apt-get install ruby1.9.1 ruby1.9.1-dev rubygems1.9.1
 
 # sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 400 \
 #      --slave /usr/share/man/man1/ruby.1.gz ruby.1.gz /usr/share/man/man1/ruby1.9.1.1.gz
